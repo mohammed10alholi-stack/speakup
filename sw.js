8a356914b2ac5e4f752b4dba8dd2c9a32427dfdf
mohammed10alholi-stack <mohammed10alholi@gmail.com>
2026-09-23 17:46:39 +0300

    cache v76

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SpeakUp service worker — يخلي الموقع يشتغل كتطبيق ويفتح بسرعة
-const CACHE = "speakup-v76";
+const CACHE = "speakup-v77";
 const CORE = ["./", "./index.html", "./config.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
