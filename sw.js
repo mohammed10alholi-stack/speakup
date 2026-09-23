6afbbdc32667aa2d5bd93c0cb0775f2830bcb88f
mohammed10alholi-stack <mohammed10alholi@gmail.com>
2026-09-23 10:07:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SpeakUp service worker — يخلي الموقع يشتغل كتطبيق ويفتح بسرعة
-const CACHE = "speakup-v70";
+const CACHE = "speakup-v71";
 const CORE = ["./", "./index.html", "./config.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
